2fab577eedce217eb561aef5aa85f645f4a71c87
Cristian Perez <cperezlecaros@gmail.com>
2025-02-11 13:48:20 -0700

    Update messages controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,20 +1,22 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dto/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages') //class decorator
 export class MessagesController {
+  constructor(private readonly messageService: MessagesService) {}
   @Get() //method decorator
   listMessages() {
-    return 'hi';
+    return this.messageService.findall();
   }
 
   @Post()
   createMessage(@Body() /*argument decorator*/ body: CreateMessageDto) {
-    console.log(body);
+    return this.messageService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log(id);
+    return this.messageService.findOne(id);
   }
 }
